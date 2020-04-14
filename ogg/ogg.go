3cca710ee1eb4c72aec6f0d1e00ce51d00d94225
Mathieu Lecarme <mlecarme@bearstech.com>
2020-04-14 23:13:31 +0200

    ogg naive implementation.

---
+++ ogg/ogg.go
@@ -0,0 +1,52 @@
+package ogg
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+)
+
+type OggReader struct {
+	reader io.Reader
+	poz    int
+	buffer []byte
+}
+
+type Page []byte
+
+func New(r io.Reader) *OggReader {
+	return &OggReader{
+		reader: r,
+		poz:    -1,
+		buffer: make([]byte, 0),
+	}
+}
+
+func (r *OggReader) Page() (Page, error) {
+	for {
+		for {
+			if len(r.buffer) == 0 {
+				break
+			}
+			poz := bytes.Index(r.buffer[r.poz+1:], []byte("OggS"))
+			fmt.Println("poz", r.poz, poz, string(r.buffer))
+			if poz == -1 {
+				break
+			}
+			if r.poz == -1 {
+				r.poz = poz
+				continue
+			}
+			page := r.buffer[r.poz : poz+1]
+			r.poz = 0
+			r.buffer = r.buffer[poz+1:]
+			return page, nil
+		}
+		chunk := make([]byte, 500*1024)
+		n, err := r.reader.Read(chunk)
+		if err != nil {
+			return nil, err
+		}
+		r.buffer = append(r.buffer, chunk[:n]...)
+	}
+}
---
+++ ogg/ogg_test.go
@@ -0,0 +1,21 @@
+package ogg
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOgg(t *testing.T) {
+	r := bytes.NewReader([]byte(`OggS_beuha_OggS_aussi_OggS`))
+	fmt.Println(r.Len())
+	o := New(r)
+	p, err := o.Page()
+	assert.NoError(t, err)
+	assert.Equal(t, "OggS_beuha_", string(p))
+	p, err = o.Page()
+	assert.NoError(t, err)
+	assert.Equal(t, "OggS_aussi_", string(p))
+}

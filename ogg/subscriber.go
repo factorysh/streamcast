1820ab6bbae6fbddb393048bf0145e5fd3b70b2f
Mathieu Lecarme <mlecarme@bearstech.com>
2020-04-22 18:45:14 +0200

    vorbis handles the pubsub, not ogg

--- ogg/subscriber.go
+++ ogg/subscriber.go
@@ -1,50 +1,6 @@
 package ogg
 
-import (
-	"context"
-	"sync"
-)
-
 type WriterFlusher interface {
 	Flush()
 	Write([]byte)
-}
-
-type Publisher struct {
-	pageWriter PageWriter
-	subscriber map[int64]WriterFlusher
-	cpt        int64
-	lock       sync.RWMutex
-}
-
-func NewPublisher(pw PageWriter) *Publisher {
-	return &Publisher{
-		pageWriter: pw,
-		subscriber: make(map[int64]WriterFlusher),
-	}
-}
-
-func (p *Publisher) Subscribe(ctx context.Context, w WriterFlusher) {
-	p.lock.Lock()
-	id := p.cpt
-	p.subscriber[id] = w
-	p.cpt++
-	p.lock.Unlock()
-	go func() {
-		select {
-		case <-ctx.Done():
-			p.lock.Lock()
-			delete(p.subscriber, id)
-			p.lock.Unlock()
-		}
-	}()
-}
-
-func (p *Publisher) WriteAllSubscribers(chunk []byte) {
-	p.lock.RLock()
-	defer p.lock.RUnlock()
-	for _, wf := range p.subscriber {
-		wf.Write(chunk)
-		wf.Flush()
-	}
 }
